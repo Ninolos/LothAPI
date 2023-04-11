52f8b51e9881020b86ce285bb6aaae1d98515402
Jonathan Daniel Loth <77141599+Ninolos@users.noreply.github.com>
2023-04-11 08:35:31 -0300

    #dev - estrutura inicial de Business e Data

---
+++ src/Loth.Business/Models/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace Loth.Business.Models
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
---
+++ src/Loth.Business/Notificacoes/Notificador.cs
@@ -0,0 +1,29 @@
+﻿using Loth.Business.Intefaces;
+
+namespace Loth.Business.Notificacoes
+{
+    public class Notificador : INotificador
+    {
+        private List<Notificacao> _notificacoes;
+
+        public Notificador()
+        {
+            _notificacoes = new List<Notificacao>();
+        }
+
+        public void Handle(Notificacao notificacao)
+        {
+            _notificacoes.Add(notificacao);
+        }
+
+        public List<Notificacao> ObterNotificacoes()
+        {
+            return _notificacoes;
+        }
+
+        public bool TemNotificacao()
+        {
+            return _notificacoes.Any();
+        }
+    }
+}
